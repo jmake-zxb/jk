7760cd4508b97e197484b39cb6491766fe8a2698
1661009855@qq.com <1661009855@qq.com>
2025-11-07 15:39:00 +0800

    first commit

--- ui/apps/web-ele/src/api/admin/system.ts
+++
@@ -1,6 +0,0 @@
-import { requestClient } from '#/api/request';
-
-// 系统缓存监控
-export function systemCache() {
-  return requestClient.get('/admin/system/cache');
-}
--- ui/packages/icons/src/iconify/index.ts
+++
@@ -1,8 +0,0 @@
-import { createIconifyIcon } from '@vben-core/icons';
-
-export * from '@vben-core/icons';
-
-export const MdiKeyboardEsc = createIconifyIcon('mdi:keyboard-esc');
-export const SolarFolderAdd = createIconifyIcon('solar:add-folder-outline');
-export const WeuiDelete = createIconifyIcon('weui:delete-outlined');
-export const CircumEdit = createIconifyIcon('circum:edit');
